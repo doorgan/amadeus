31cfe2e89b006acc6485800ac036fd72a00c8d69
doorgan <dorgandash@gmail.com>
2021-12-19 00:59:08 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amadeus.MixProject do
   def project do
     [
       app: :amadeus,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
